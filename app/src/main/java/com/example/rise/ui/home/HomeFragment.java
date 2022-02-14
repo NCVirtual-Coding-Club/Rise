0c1a815f54713bf603ccf52810612ac6680c855c
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2022-02-14 05:04:55 -0500

    Add adaptive welcome message support to @+id/text_home_greetingWelcomeText

--- app/src/main/java/com/example/rise/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/rise/ui/home/HomeFragment.java
@@ -1,5 +1,8 @@
 package com.example.rise.ui.home;
 
+import static com.example.rise.R.id.text_home_greetingWelcomeText;
+
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,13 +10,14 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.rise.R;
 import com.example.rise.databinding.FragmentHomeBinding;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class HomeFragment extends Fragment {
 
@@ -22,6 +26,8 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
@@ -30,6 +36,43 @@ public class HomeFragment extends Fragment {
 
         return root;
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        System.out.println("Test");
+
+
+        // Start: Controls the Welcome Message on the text_home_greetingWelcomeText TextView
+
+        // gets hour using java.time
+        LocalDateTime myDateObj = LocalDateTime.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("HH");
+        int date = Integer.parseInt(myDateObj.format(myFormatObj));
+
+        // gets user's name
+        TextView welcomeText = null;
+        String userSName = new String("Jonathan");
+        welcomeText = (TextView) view.findViewById(text_home_greetingWelcomeText);
+
+        // if there is no name
+        if (userSName == null) {
+            userSName = "";
+        }
+
+        // sets text based on hour
+        if (date < 12) {
+            welcomeText.setText("Good Morning " + userSName);
+        } else if (date < 18) {
+            welcomeText.setText("Good Afternoon " + userSName);
+        } else {
+            welcomeText.setText("Good Evening " + userSName);
+        }
+        // End
+
+    }
+
 
     @Override
     public void onDestroyView() {
