29b5d211ad2735e73488d61b90e4a5724ae63e1b
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2022-02-20 13:02:35 -0500

    Add intent to dial on SupportFragment.java

--- app/src/main/java/com/example/rise/ui/support/SupportFragment.java
+++ app/src/main/java/com/example/rise/ui/support/SupportFragment.java
@@ -1,13 +1,21 @@
 package com.example.rise.ui.support;
 
+import static com.example.rise.R.id.card_support_emergency;
+import static com.example.rise.R.id.card_support_substance;
+import static com.example.rise.R.id.card_support_suicide;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -35,6 +43,58 @@ public class SupportFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // The following code handles OnClick events for all cards on the support page
+        // Start
+
+        // The following code stores CardViews for each card
+        CardView card_suicide = (CardView) view.findViewById(card_support_suicide);
+        CardView card_substance = (CardView) view.findViewById(card_support_substance);
+        CardView card_emergency = (CardView) view.findViewById(card_support_emergency); // creating a CardView and assigning a value.
+
+        // Phone numbers
+        String phoneNumberSuicide = "8002738255";
+        String phoneNumberSubstance = "8777264727";
+        String phoneNumberEmergency = "911";
+
+        // On Click (Suicide) . . . call number
+        card_suicide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do whatever you want to do on click (to launch any fragment or activity you need to put intent here.)
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phoneNumberSuicide)); // This should not be needed (in theory) as the application should already have the number.
+                startActivity(intent);
+            }
+        });
+
+        // On Click (Substance) . . . call number
+        card_substance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do whatever you want to do on click (to launch any fragment or activity you need to put intent here.)
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phoneNumberSubstance)); // This should not be needed (in theory) as the application should already have the number.
+                startActivity(intent);
+            }
+        });
+
+        // On Click (Emergency) . . . call number
+        card_emergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // do whatever you want to do on click (to launch any fragment or activity you need to put intent here.)
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phoneNumberEmergency)); // This should not be needed (in theory) as the application should already have the number.
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
