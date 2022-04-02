856db2a5623cc65c00a51da35b3e7065b89246ba
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2022-04-02 15:57:51 -0400

    Add diseasesVaccines

--- app/src/main/java/com/example/rise/ui/learn/LearnFragment.java
+++ app/src/main/java/com/example/rise/ui/learn/LearnFragment.java
@@ -1,17 +1,20 @@
 package com.example.rise.ui.learn;
 
+import static com.example.rise.R.id.card_learn_disease;
+import static com.example.rise.R.id.card_learn_drugs;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.example.rise.R;
 import com.example.rise.databinding.FragmentLearnBinding;
 
 public class LearnFragment extends Fragment {
@@ -27,14 +30,26 @@ public class LearnFragment extends Fragment {
         binding = FragmentLearnBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        learnViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
+        return root;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        CardView drugsAlcohol = (CardView) view.findViewById(card_learn_drugs);
+        drugsAlcohol.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_navigation_learn_to_drugsAlcohol);
             }
         });
-        return root;
+
+        CardView diseases = (CardView) view.findViewById(card_learn_disease);
+        diseases.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_navigation_learn_to_diseasesVaccines);
+            }
+        });
     }
 
     @Override
